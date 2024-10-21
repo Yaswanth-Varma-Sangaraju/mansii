6ad60aa5a3b75cdfa5f10654d3649c97f799c5d2
Yaswanth-Varma-Sangaraju <150851499+Yaswanth-Varma-Sangaraju@users.noreply.github.com>
2024-10-21 23:04:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ const scrollUpButton = document.querySelector('.scroll-up-button');
 scrollUpButton.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+window.onload = function() {
+    var preloader = document.getElementById('preloader');
+    var loadingBar = document.getElementById('loading-bar');
+    var percentage = document.getElementById('percentage');
+    var progress = 0;
+  
+    var interval = setInterval(function() {
+      progress += 5;
+      percentage.textContent = progress + '%';
+      loadingBar.style.width = progress + '%';
+  
+      if (progress >= 100) {
+        clearInterval(interval);
+        preloader.style.display = 'none';
+      }
+    }, 50);
+  };
